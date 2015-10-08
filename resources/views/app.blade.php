40aa0c3938c978512103ec3feecfbb4e27c70848
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-10-08 17:41:47 +0200

    Rebranding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>OnePage CMS - @yield('title')</title>
+		<title>Monster CMS - @yield('title')</title>
 
 		<link href="/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/css/app.css" rel="stylesheet">
