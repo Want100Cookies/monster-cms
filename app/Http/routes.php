13dac448b08dd8cbbfc3ef6ff3f5b47f67b83510
Pascal Drewes <pascal@drewes-webdesign.nl>
2017-08-08 07:37:39 +0000

    Apply fixes from StyleCI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,23 +11,23 @@
 |
 */
 
-Route::controller(  'auth',                     'Auth\AuthController');
-Route::controller(  'password',                 'Auth\PasswordController');
+Route::controller('auth', 'Auth\AuthController');
+Route::controller('password', 'Auth\PasswordController');
 
-Route::get(         '/panel',                   'PanelController@index');
+Route::get('/panel', 'PanelController@index');
 
-Route::get(         '/panel/page/create',       'PanelController@create');
-Route::post(        '/panel/page/create',       'PanelController@store');
+Route::get('/panel/page/create', 'PanelController@create');
+Route::post('/panel/page/create', 'PanelController@store');
 
-Route::get(			'/panel/block/create/{pageId}/{blockType}', 'PanelController@createBlock');
-Route::post(        '/panel/block/store',		'PanelController@storeBlock');
-Route::get(			'/panel/block/edit/{blockId}', 'PanelController@editBlock');
-Route::post(        '/panel/block/update',		'PanelController@updateBlock');
-Route::post(        '/panel/block/get',			'PanelController@getBlock');
-Route::post(        '/panel/block/destroy',     'PanelController@destroyBlock');
+Route::get('/panel/block/create/{pageId}/{blockType}', 'PanelController@createBlock');
+Route::post('/panel/block/store', 'PanelController@storeBlock');
+Route::get('/panel/block/edit/{blockId}', 'PanelController@editBlock');
+Route::post('/panel/block/update', 'PanelController@updateBlock');
+Route::post('/panel/block/get', 'PanelController@getBlock');
+Route::post('/panel/block/destroy', 'PanelController@destroyBlock');
 
-Route::get(         '/panel/page/{slug}/edit',      'PanelController@edit');
-Route::post(        '/panel/page/{slug}/update',    'PanelController@update');
+Route::get('/panel/page/{slug}/edit', 'PanelController@edit');
+Route::post('/panel/page/{slug}/update', 'PanelController@update');
 
-Route::any(         '/',                        'PageController@show');
-Route::any(         '{slug}',                   'PageController@show')->where('slug', '[a-z0-9-]+');
+Route::any('/', 'PageController@show');
+Route::any('{slug}', 'PageController@show')->where('slug', '[a-z0-9-]+');
